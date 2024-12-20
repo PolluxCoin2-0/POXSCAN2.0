1b320ecd688066ed5a206d2c345289f247fa23de
shubhampoxscan <shubham.gupta@poxscan.io>
2024-12-20 15:24:03 +0530

    code pushed

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,10 +1,10 @@
 const PageNotFound = () => {
   return (
-    <div className="h-[80vh] bg-gray-50 flex items-center">
+    <div className="h-[80vh] bg-black flex items-center">
       <div className="container flex flex-col md:flex-row items-center justify-between px-5 text-gray-700">
         <div className="w-full lg:w-1/2 mx-8">
           <div className="text-7xl text-[#38DD02] font-dark font-extrabold mb-8">404</div>
-          <p className="text-2xl md:text-2xl font-semibold leading-normal mb-8">
+          <p className="text-2xl md:text-2xl font-semibold leading-normal mb-8 text-white">
             Sorry, we couldn't find the page you're looking for
           </p>
           <a
